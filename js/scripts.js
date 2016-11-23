f733b93c99be93732a4e6c36788d691eefa9c07e
Ayana Powell <ayanapowell1@gmail.com>
2016-11-22 16:29:06 -0800

    update this landing page with jquery animations

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,45 @@
-// $(document).ready(function() {
-//   $('#wrapper').wrap('<div class="overlay"></div>');
-// });
+$(document).ready(function() {
+  $('.about-button').click(function() {
+    showProfile();
+  });
+
+  $('img.close').click(function() {
+    hideProfile();
+  });
+
+  function showProfile() {
+    $('.toggled-body').animate({
+      left: '-150%',
+    }, 800);
+    setTimeout(function() {
+      $('.overlay').css({
+        "background": "rgba(0, 0, 0, 0.8)"
+      })
+      $('#profile').animate({
+        right: '0',
+      }, 500);
+    });
+  }
+
+  function hideProfile() {
+    $('#profile').animate({
+      height: '1px',
+    }, 500);
+    setTimeout(function() {
+      $('#profile').fadeOut(80);
+    });
+    setTimeout(function() {
+      $('.overlay').css({
+        "background": "rgba(0, 0, 0, 0.6)"
+      })
+      $('.toggled-body').animate({
+        left: '0'
+      }, 500)
+    })
+    setTimeout(function() {
+      location.reload();
+    }, 560)
+  }
+
+});
 
